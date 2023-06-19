41f89a75390d54e2fb176f9d4d468c77562a4f2c
Gwilyn <gwilyn@karmabunny.com.au>
2023-06-20 08:46:47 +0930

    Disable/remove whatever skin bits I don't care about.
    Tbh we're not using anything in the skin at all.

--- src/skin/sprout_load.php
+++ src/skin/sprout_load.php
@@ -4,10 +4,10 @@ use Sprout\Helpers\Register;
 
 
 // Convert file/download/id(/size) links to actual file names e.g. 'files/123_example.jpg'
-Register::contentReplace('main_content', ['Sprout\\Helpers\\ContentReplace', 'fileDownload']);
+// Register::contentReplace('main_content', ['Sprout\\Helpers\\ContentReplace', 'fileDownload']);
 
 // Update document links to have a cache-buster and also Google event tracking
-Register::contentReplace('main_content', ['Sprout\\Helpers\\Sprout', 'specialFileLinks']);
+// Register::contentReplace('main_content', ['Sprout\\Helpers\\Sprout', 'specialFileLinks']);
 
 // Determine if there is a rel="facebox" in use and if so, load the Facebox library
 Register::contentReplace('inner_html', function($html) {
@@ -15,8 +15,4 @@ Register::contentReplace('inner_html', function($html) {
         Needs::fileGroup('magnific_popup');
     }
     return $html;
-});
-
-Register::templateVariable('hello', function() {
-    return 'Hello World!';
 });

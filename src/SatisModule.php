654aae1d3086e04678c23fb79d4852327cb0ccda
Gwilyn <gwilyn@karmabunny.com.au>
2024-05-07 14:56:08 +1000

    Add satis module class.

---
+++ src/SatisModule.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Copyright (C) 2023 Karmabunny Pty Ltd.
+ */
+
+namespace SproutModules\Karmabunny\Satis;
+
+use Sprout\Helpers\Module;
+use Sprout\Helpers\Sprout;
+
+class SatisModule extends Module
+{
+    /** @inheritdoc */
+    public function getVersion(): string
+    {
+        return Sprout::getInstalledVersion('karmabunny/satis');
+    }
+}
